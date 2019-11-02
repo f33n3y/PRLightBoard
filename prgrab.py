9bdac6a174d763090b7b1a171f017f3f8ae01dbe
f33n3y <f33n3y_a@hotmail.co.uk>
2019-11-02 14:01:04 +0000

    removed commented code

--- prgrab.py
+++ prgrab.py
@@ -1,4 +1,3 @@
-
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from azure.devops.v5_1.git.models import GitPullRequestSearchCriteria
@@ -15,29 +14,23 @@ core_client = connection.clients.get_core_client()
 get_projects_response = core_client.get_projects()
 index = 0
 while get_projects_response is not None:
-   for project in get_projects_response.value:
-       projects_list.append(project)
-       index += 1
-   if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
-       # Get the next page of projects
-       get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
-   else:
-       # All projects have been retrieved
-       get_projects_response = None
-
-   # Get repos
-   # repo_list = []
-   # repos = git_client.get_repositories("xxx")
-   # for repo in repos:
-   #     repo_list.append(repo)
+    for project in get_projects_response.value:
+        projects_list.append(project)
+        index += 1
+    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+        # Get the next page of projects
+        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
+    else:
+        # All projects have been retrieved
+        get_projects_response = None
 
 total_prs_count = 0
 git_client = connection.clients.get_git_client()
 search_criteria = GitPullRequestSearchCriteria()
 
 for project in projects_list:
-   projects_pull_requests = git_client.get_pull_requests_by_project(project.id, search_criteria)
-   print("PRs for " + project.name + " = " + str(len(projects_pull_requests)))
-   total_prs_count += len(projects_pull_requests)
+    projects_pull_requests = git_client.get_pull_requests_by_project(project.id, search_criteria)
+    print("PRs for " + project.name + " = " + str(len(projects_pull_requests)))
+    total_prs_count += len(projects_pull_requests)
 
 print("Total PRs: " + str(total_prs_count))
